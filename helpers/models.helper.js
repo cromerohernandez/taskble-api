519e52524b64e22fa10387b08ff3060c97f3d602
cromerohernandez <c.romero.hsv@gmail.com>
2021-01-17 12:37:55 +0100

    dev: user.model & models.helper

---
+++ helpers/models.helper.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt')
+
+const SALT_WORK_FACTOR = 10
+
+function checkPassword (password, user) {
+  return bcrypt.compare(password, user.password)
+}
+
+function checkPasswordFormat (password) {
+  const letters = 'abcdefghijklmnñopqrstuvwxyz'
+  const numbers = '0123456789'
+  var check = {
+    letters: false,
+    numbers: false
+  }
+  password = password.toLowerCase()
+  for (i=0; i<password.length; i++) {
+    if (letters.indexOf(password.charAt(i))!=-1) {
+      check.letters = true
+      continue
+    }
+  }
+  for (i=0; i<password.length; i++) {
+    if (numbers.indexOf(password.charAt(i))!=-1) {
+      check.numbers = true
+    }
+  }
+  return (check.letters & check.numbers) ? true : false
+}
+
+function generateRandomToken () {
+  const randomString = () => Math.random().toString(36).substring(2, 13)
+  return randomString() + randomString() + randomString() + randomString()
+}
+
+function hashPassword (next, user) {
+  if (user.isModified('password')) {
+    bcrypt.genSalt(SALT_WORK_FACTOR)
+      .then(salt => {
+        return bcrypt.hash(user.password, salt)
+          .then(hash => {
+            user.password = hash
+            next()
+          })
+      })
+      .catch(error => next(error))
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  checkPassword,
+  checkPasswordFormat,
+  generateRandomToken,
+  hashPassword
+}
---
+++ models/user.model.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+
+const { checkPassword, checkPasswordFormat, generateRandomToken, hashPassword } = require('../helpers/models.helper')
+
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+const userSchema = new.mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+    unique: true,
+    immutable: true,
+    trim: true,
+    lowercase: true,
+    match: [EMAIL_PATTERN, 'email format is invalid']
+  },
+  password: {
+    type: String,
+    required: [
+      [true, 'password is required'],
+      [checkPasswordFormat, 'the password must contains numbers and letters']
+    ],
+    minlength: [8, 'the password needs at least 8 chars']
+  },
+  validationToken: {
+    type: String,
+    default: generateRandomToken
+  },
+  validated: {
+    type: Boolean,
+    default: false
+  },
+  lastAccess: {
+    type: Date,
+    default: Date.now()
+  }
+},
+{ timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc._id;
+      delete ret._id;
+      delete ret.__v;
+      delete ret.password;
+      delete ret.validationToken;
+      return ret;
+    }
+  }
+})
+
+userSchema.pre('save', function (next) {
+  hashPassword(next, this)
+})
+
+userSchema.methods.checkUserPassword = function (password) {
+  return checkPassword(password, this)
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
