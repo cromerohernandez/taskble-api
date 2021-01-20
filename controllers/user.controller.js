c8666f4b3595bb4d89bf9ff5efd2858ae0b3e894
cromerohernandez <c.romero.hsv@gmail.com>
2021-01-20 23:43:31 +0100

    ADD: user.controller.validate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,7 @@ const mailer = require('../config/mailer.config')
 
 const User = require('../models/user.model')
 
-//const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000'
+const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000'
 
 module.exports.create = (req, res, next) => {
   const { email, password } = req.body
@@ -17,6 +17,23 @@ module.exports.create = (req, res, next) => {
     .then(user => {
       mailer.sendValidateUserEmail(user)
       res.status(201).json(user)
+    })
+    .catch(next)
+}
+
+module.exports.validate = (req, res, next) => {
+  User.findOne({ validationToken: req.params.token })
+    .then(user => {
+      if(user) {
+        user.validated = true
+        user.save()
+          .then(() => {
+            res.status(200).redirect(`${CORS_ORIGIN}/`)
+          })
+          .catch(next)
+      } else {
+        throw createError(404, 'User not found')
+      }
     })
     .catch(next)
 }
